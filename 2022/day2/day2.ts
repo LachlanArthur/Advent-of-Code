7c51b87fe992523f8fecc3d0a51b1b8f22fb8603
Lachlan Arthur <git@me.lach.la>
2022-12-03 11:59:00 +1000

    2022 day 2

---
+++ 2022/day2/day2.ts
@@ -0,0 +1,88 @@
+import { sum } from '../helpers';
+
+import example from './example';
+import input from './input';
+
+const scores = {
+	// Rock
+	A: {
+		// Rock
+		X: 1 + 3,
+		// Paper
+		Y: 2 + 6,
+		// Scissors
+		Z: 3 + 0,
+	},
+	// Paper
+	B: {
+		// Rock
+		X: 1 + 0,
+		// Paper
+		Y: 2 + 3,
+		// Scissors
+		Z: 3 + 6,
+	},
+	// Scissors
+	C: {
+		// Rock
+		X: 1 + 6,
+		// Paper
+		Y: 2 + 0,
+		// Scissors
+		Z: 3 + 3,
+	},
+}
+
+const part1 = ( input: string ) => sum(
+	input.split( '\n' )
+		.map( line => {
+			const [ them, us ] = line.split( ' ' );
+			return scores[ them ][ us ]
+		} )
+);
+
+console.log( part1( example ) )
+
+console.log( part1( input ) )
+
+const outcomes = {
+	// Rock
+	A: {
+		// Lose
+		X: 0 + 3, // Scissors
+		// Draw
+		Y: 3 + 1, // Rock
+		// Win
+		Z: 6 + 2, // Paper
+	},
+	// Paper
+	B: {
+		// Lose
+		X: 0 + 1, // Rock
+		// Draw
+		Y: 3 + 2, // Paper
+		// Win
+		Z: 6 + 3, // Scissors
+	},
+	// Scissors
+	C: {
+		// Lose
+		X: 0 + 2, // Paper
+		// Draw
+		Y: 3 + 3, // Scissors
+		// Win
+		Z: 6 + 1, // Rock
+	},
+}
+
+const part2 = ( input: string ) => sum(
+	input.split( '\n' )
+		.map( line => {
+			const [ them, us ] = line.split( ' ' );
+			return outcomes[ them ][ us ]
+		} )
+)
+
+console.log( part2( example ) )
+
+console.log( part2( input ) )
