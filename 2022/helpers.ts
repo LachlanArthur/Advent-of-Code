894efe9d0f571878dd6e578fba9c57428dd89697
Lachlan Arthur <git@me.lach.la>
2022-12-05 11:30:33 +1000

    Helper method to chunk iterables

--- 2022/helpers.ts
+++ 2022/helpers.ts
@@ -13,3 +13,29 @@ export function findSharedLetter( [ first, ...rest ]: string[] ): string {
 		.split( '' )
 		.filter( letter => rest.every( item => item.includes( letter ) ) )[0]
 }
+
+export function* chunks<T>( items: Iterable<T>, size: number, offset = 0 ) {
+	const iterator = items[ Symbol.iterator ]();
+	let lastN: T[] = [];
+
+	while ( true ) {
+		const { done, value } = iterator.next();
+		if ( done ) break;
+
+		lastN.push( value );
+
+		if ( lastN.length === size ) {
+			yield lastN.slice( 0, size );
+
+			lastN.splice( 0, size + offset );
+
+			for ( let skip = 0; skip < Math.max( 0, offset ); skip++ ) {
+				iterator.next();
+			}
+		}
+	}
+
+	if ( lastN.length ) {
+		yield lastN;
+	}
+}
