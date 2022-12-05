200abdb10cfd66bd3580152e4669654e98e087eb
Lachlan Arthur <git@me.lach.la>
2022-12-05 11:31:08 +1000

    template

---
+++ 2022/dayX/dayX.ts
@@ -0,0 +1,20 @@
+import { chunker, findSharedLetter, sum } from '../helpers';
+
+import example from './example';
+import input from './input';
+
+function part1( input: string ) {
+	
+}
+
+console.assert( part1( example ) === ? );
+
+console.log( part1( input ) );
+
+// function part2( input: string ) {
+
+// }
+
+// console.assert( part2( example ) === ? );
+
+// console.log( part2( input ) );
