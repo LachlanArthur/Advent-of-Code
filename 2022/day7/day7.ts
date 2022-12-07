637e81c098d40868488ff24c9a7ddeb360198bcc
Lachlan Arthur <git@me.lach.la>
2022-12-07 16:25:27 +1000

    2022 day 7

---
+++ 2022/day7/day7.ts
@@ -0,0 +1,117 @@
+import { sum } from '../helpers';
+
+import example from './example';
+import input from './input';
+
+function parse( input: string ) {
+	const fileSizes = new Map<string, number>();
+	let currentPath = '';
+
+	for ( const section of input.split( '$ ' ).filter( String ) ) {
+		const lines = section.split( '\n' );
+		const [ command, params ] = lines.shift()!.split( ' ' );
+		const output = lines.filter( String );
+
+		switch ( command ) {
+
+			case 'cd':
+				switch ( params ) {
+					default: currentPath += params + '/'; break;
+					case '/': currentPath = '/'; break;
+					case '..': currentPath = currentPath.replace( /[^\/]+\/$/, '' ); break;
+				}
+				break;
+
+			case 'ls':
+				for ( const entry of output ) {
+					if ( entry.startsWith( 'dir ' ) ) {
+						continue;
+					} else {
+						const [ filesizeString, filename ] = entry.split( ' ' );
+
+						fileSizes.set( currentPath + filename, parseInt( filesizeString ) );
+					}
+				}
+				break;
+
+		}
+	}
+
+	return {
+		fileSizes,
+		directorySizes: directorySizes( fileSizes ),
+	};
+}
+
+function directorySizes( filesizes: Map<string, number> ): Map<string, number> {
+	const directories = new Set<string>();
+
+	for ( const [ file ] of filesizes ) {
+		let parts = file.split( '/' );
+		while ( parts.pop() ) {
+			directories.add( parts.join( '/' ) + '/' );
+		}
+	}
+
+	const directorySizes = new Map<string, number>(
+		Array.from( directories.values() )
+			.map( dir => [ dir, 0 ] )
+	);
+
+	for ( const [ file, fileSize ] of filesizes ) {
+		for ( const [ dir ] of directorySizes ) {
+			if ( file.startsWith( dir ) ) {
+				directorySizes.set( dir, directorySizes.get( dir )! + fileSize );
+			}
+		}
+	}
+
+	return directorySizes;
+}
+
+function part1( input: string ): number {
+	const {
+		fileSizes,
+		directorySizes,
+	} = parse( input );
+
+	// console.table( filesizes );
+	// console.table( directorySizes );
+
+	return sum(
+		Array.from( directorySizes.entries() )
+			.filter( ( [ path, size ] ) => size <= 100000 )
+			.map( ( [ path, size ] ) => size )
+	);
+}
+
+console.assert( part1( example ) === 95437 );
+
+console.log( part1( input ) );
+
+function part2( input: string ) {
+	const {
+		fileSizes,
+		directorySizes,
+	} = parse( input );
+
+	// console.table( filesizes );
+	// console.table( directorySizes );
+
+	const driveSize = 70000000;
+	const needed = 30000000;
+	const used = directorySizes.get( '/' )!;
+	const unused = driveSize - used;
+	const toDelete = needed - unused;
+
+	const largeDirs = Array.from( directorySizes.entries() )
+		.filter( ( [ path, size ] ) => size >= toDelete );
+
+	return largeDirs
+		.sort( ( [ pathA, sizeA ], [ pathB, sizeB ] ) => sizeA - sizeB )[ 0 ][ 1 ];
+
+}
+
+console.assert( part2( example ) === 24933642 );
+
+console.log( part2( input ) );
