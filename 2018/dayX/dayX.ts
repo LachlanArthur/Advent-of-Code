cb7bd03e2b9d87c1198b2f440b81e096b2c49b7e
Lachlan Arthur <git@me.lach.la>
2023-11-15 11:41:52 +1000

    feat: 2018 template

---
+++ 2018/dayX/dayX.ts
@@ -0,0 +1,23 @@
+import '../../extensions.ts';
+import { bench } from '../../bench.ts';
+
+import example from './example.ts';
+import input from './input.ts';
+
+function part1( input: string ) {
+	return input
+		.split( '\n' )
+		.log()
+}
+
+bench( 'part 1 example', () => part1( example ), undefined );
+
+bench( 'part 1 input', () => part1( input ) );
+
+// function part2( input: string ) {
+
+// }
+
+// bench( 'part 2 example', () => part2( example ), undefined );
+
+// bench( 'part 2 input', () => part2( input ) );
