bfcd5d1ef75711d1d621bdd27db46282b0a1e39c
Lachlan Arthur <git@me.lach.la>
2023-11-19 01:03:14 +1000

    docs: Improve manual instructions

--- 2018/day10/day10.ts
+++ 2018/day10/day10.ts
@@ -9,13 +9,15 @@ function part1( input: string ) {
 		.lines()
 		.map( line => line.match( /-?\d+/g )!.map( Number ) );
 
-	console.log( 'Paste this in to https://www.desmos.com/calculator\n' );
-	console.log( 't=0' );
-	console.log(
+	return 'Paste this in to https://www.desmos.com/calculator\n' +
+		'Zoom out until you can see all the points\n' +
+		'drag them inwards until the message appears\n' +
+		'\n' +
 		particles
 			.map( ( [ x, y, dx, dy ] ) => `( ${dx}t + ${x} , ${-dy}t + ${-y} )` )
-			.join( '\n' )
-	);
+			.join( '\n' ) +
+		'\n' +
+		't = 0\n';
 }
 
 bench( 'part 1 input', () => part1( input ) );
