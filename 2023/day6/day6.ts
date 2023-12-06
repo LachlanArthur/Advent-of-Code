dfb0a9dd879f05c4d9b3009c974a0942a4385ccf
Lachlan Arthur <git@me.lach.la>
2023-12-06 15:40:54 +1000

    refactor: Some simplification

--- 2023/day6/day6.ts
+++ 2023/day6/day6.ts
@@ -5,24 +5,22 @@ import example from './example.ts';
 import input from './input.ts';
 
 function part1( input: string ) {
-	const races = input
+	return input
 		.lines()
-		.map( line => line.match( /-?\d+/g )!.map( Number ) )
+		.map( line => line.match( /\d+/g )!.map( Number ) )
 		.transpose()
-		.log() as [ number, number ][]
+		.map( ( [ time, distance ] ) => {
+			let wins = 0;
 
-	return races.map( ( [ time, distance ] ) => {
-		let wins = 0;
-
-		for ( let speed = 1; speed < time - 1; speed++ ) {
-			const remaining = time - speed;
-			if ( remaining * speed > distance ) {
-				wins++;
+			for ( let speed = 1; speed < time - 1; speed++ ) {
+				const remaining = time - speed;
+				if ( remaining * speed > distance ) {
+					wins++;
+				}
 			}
-		}
 
-		return wins;
-	} )
+			return wins;
+		} )
 		.product();
 }
 
@@ -31,22 +29,9 @@ bench( 'part 1 example', () => part1( example ), 288 );
 bench( 'part 1 input', () => part1( input ) );
 
 function part2( input: string ) {
-	const [ time, distance ] = input
-		.lines()
-		.map( line => Number( line.replace( /\D/g, '' ) ) )
-
-	let wins = 0;
-
-	for ( let speed = 1; speed < time - 1; speed++ ) {
-		const remaining = time - speed;
-		if ( remaining * speed > distance ) {
-			wins++;
-		}
-	}
-
-	return wins;
+	return part1( input.replaceAll( ' ', '' ) );
 }
 
-bench( 'part 2 example', () => part2( example ), undefined );
+bench( 'part 2 example', () => part2( example ), 71503 );
 
 bench( 'part 2 input', () => part2( input ) );
