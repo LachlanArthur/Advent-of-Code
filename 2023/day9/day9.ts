663bb95ced5abc80a2c4b52ee5e222e5d0fcc0d9
Lachlan Arthur <git@me.lach.la>
2023-12-10 05:23:34 +1000

    refactor: Use bench100

--- 2023/day9/day9.ts
+++ 2023/day9/day9.ts
@@ -1,5 +1,5 @@
 import { } from '../../extensions.ts';
-import { bench } from '../../bench.ts';
+import { bench, bench100 } from '../../bench.ts';
 import { Polynomial } from '../../maths.ts';
 
 import example from './example.ts';
@@ -16,9 +16,9 @@ function part1( input: string ) {
 		.sum()
 }
 
-bench( 'part 1 example', () => part1( example ), 114n );
+bench100( 'part 1 example', () => part1( example ), 114n );
 
-bench( 'part 1 input', () => part1( input ) );
+bench100( 'part 1 input', () => part1( input ) );
 
 function part2( input: string ) {
 	return input
@@ -31,6 +31,6 @@ function part2( input: string ) {
 		.sum()
 }
 
-bench( 'part 2 example', () => part2( example ), 2n );
+bench100( 'part 2 example', () => part2( example ), 2n );
 
-bench( 'part 2 input', () => part2( input ) );
+bench100( 'part 2 input', () => part2( input ) );
