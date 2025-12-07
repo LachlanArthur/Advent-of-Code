7af9eff8383261b2d68b73b9e0f9abb2499a46ad
Lachlan Arthur <git@me.lach.la>
2025-12-07 17:15:28 +1000

    Funny eval solution

--- 2025/day6/day6.ts
+++ 2025/day6/day6.ts
@@ -57,3 +57,46 @@ function part2( input: string ) {
 bench( 'part 2 example', () => part2( example ), 3263827 );
 
 bench( 'part 2 input', () => part2( input ) );
+
+function part2eval( input: string ) {
+	const lines = input.split( '\n' );
+	const maxY = lines.length;
+	const maxX = lines[ 0 ].length;
+
+	let formula = '';
+	let operator = '';
+	let writeOperator = false;
+	for ( let x = 0; x < maxX; x++ ) {
+		let gap = true;
+		for ( let y = 0; y < maxY; y++ ) {
+			const char = lines[ y ][ x ];
+			switch ( char ) {
+				default:
+					if ( writeOperator ) {
+						formula += operator;
+						writeOperator = false;
+					}
+					formula += char;
+					gap = false;
+					break;
+				case '*': case '+':
+					operator = char;
+				case ' ':
+					if ( y === maxY - 1 ) {
+						if ( gap ) {
+							formula += ')+(';
+							writeOperator = false;
+						} else {
+							writeOperator = true;
+						}
+					}
+			}
+		}
+	}
+
+	return eval( '(' + formula + ')' );
+}
+
+bench( 'part 2 eval example', () => part2eval( example ), 3263827 );
+
+bench( 'part 2 eval input', () => part2eval( input ) );
